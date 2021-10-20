d799be8abbb0a0a196809accaa4f5a86ae3df997
ramhors <rjm2075@gmail.com>
2021-10-20 12:47:54 -0400

    adding comments to the file

--- school-registration/src/com/revature/app/Main.java
+++ school-registration/src/com/revature/app/Main.java
@@ -5,8 +5,10 @@ import com.revature.model.Teacher;
 
 public class Main {
 	
+	
 	public static void main(String[] args) {
 		
+		//Creating objects for the teacher and student
 		Teacher teacher = new Teacher("Smith","Ben","5B",2000,700);
 		Student student = new Student("Patrick","Rob",24,"A");
 		
